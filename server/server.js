9cf2118cac84842d83269b0ffbda795d6995dad6
Het-Mewada <hetmwd1384@gmail.com>
2026-03-21 23:16:36 +0530

    removed alternative port

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ import { startDayLockCron } from './cron/dayLock.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Middleware
 const corsOptions = {
